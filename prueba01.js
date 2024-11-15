fe832a8fa8944713a44786ce6ba86e44e95fd76a
Francisco V. Diaz <fevarga1@uc.cl>
2024-11-15 20:13:28 -0300

    test01 completed

--- prueba01.js
+++ prueba01.js
@@ -83,14 +83,14 @@ class Project {
     estado.
      */
     showSummary() {
-        console.table({
+        return {
             "ID": this.id,
             "Project": this.name,
             "Start Date": this.startDate.toLocaleString(),
             "Pending Tasks": this.showTasksStatus("Pending"),
             "Completed Tasks": this.showTasksStatus("Completed"),
             "In Progress Tasks": this.showTasksStatus("In Progress"),
-        });
+        };
     }
 
     showTasksStatus(status) {
@@ -103,22 +103,21 @@ class Project {
      */
     sortTasksByDeadline() {
         this.tasks.sort((a, b) => a.deadline - b.deadline);
-        console.table(this.tasks);
+        return this.tasks;
     }
 
     /*
-    Implementa una función calcularTiempoRestante que utilice el método
+    Implementa una función calcularTiempoRestante que utilice la funcion
     reduce para calcular el número total de días que faltan para completar todas
     las tareas pendientes de un proyecto.
      */
     calculateRemainingTime() {
         const currentDate = new Date();
         const pendingTasks = this.tasks.filter(task => task.status === "Pending");
-        const remainingDays = pendingTasks.reduce((totalDays, task) => {
+        return pendingTasks.reduce((totalDays, task) => {
             const days = Math.ceil((task.deadline - currentDate) / (1000 * 60 * 60 * 24));
             return totalDays + days;
         }, 0);
-        console.log(`Remaining days: ${remainingDays}`);
     }
 
     /*
@@ -128,23 +127,20 @@ class Project {
      */
     getCriticalTasks() {
         const currentDate = new Date();
-        const criticalTasks = this.tasks.filter(task => {
+        return this.tasks.filter(task => {
             const days = Math.ceil((task.deadline - currentDate) / (1000 * 60 * 60 * 24));
             return days < 3 && days >= 0 && task.status !== "Completed";
         });
-        console.table(criticalTasks);
     }
 
-
-}
-
-/*
-Crea una función de orden superior filtrarTareasProyecto que tome una
-función de filtrado como argumento y la aplique a la lista de tareas de un
-proyecto.
- */
-function showFilteredTasks(project, filterFunction, query) {
-    console.table(filterFunction(project.tasks, query));
+    /*
+    Crea una función de orden superior filtrarTareasProyecto que tome una
+    función de filtrado como argumento y la aplique a la lista de tareas de un
+    proyecto.
+     */
+    showFilteredTasks(filterFunction, query) {
+        return filterFunction(this.tasks, query)
+    }
 }
 
 function filterTasksByStatus(tasks, status) {
@@ -164,8 +160,8 @@ async function loadProjectDetails(project) {
     console.log("Loading project details...");
     try {
         const showLoadedProject = await fetchProject(project);
-        showLoadedProject();
         console.log("Project details loaded!");
+        return showLoadedProject();
     } catch (error) {
         console.error(error);
     }
@@ -183,21 +179,52 @@ function fetchProject(project) {
 /*
     PRUEBA DE LOS MÉTODOS
  */
+
+// CREAR PROYECTO Y AÑADIR TAREAS
 const project = new Project("Project Test");
 project.addNewTask("Give christmas presents!", new Date(2024, 11, 25), "Completed");
 project.addNewTask("New year PARTY!", new Date(2024, 11, 31), "Pending");
 project.addNewTask("Buy new car", new Date(2024, 10, 4), "In Progress");
 project.addNewTask("Buy new house", new Date(2024, 9, 17), "Pending");
-project.showSummary();
-project.sortTasksByDeadline();
 
-showFilteredTasks(project, filterTasksByStatus, "Completed");
-showFilteredTasks(project, filterTasksByStatus, "In Progress");
-showFilteredTasks(project, filterTasksBySearch, "buy");
-showFilteredTasks(project, filterTasksBySearch, "Christmas");
+// MOSTRAR RESUMEN DEL PROYECTO
+console.log(`
+Mostrar resumen del proyecto. Debería mostrar 1 tarea completada, 1 en progreso y 2 pendientes.
+`);
+console.table(project.showSummary());
 
-project.calculateRemainingTime();
+// ORDENAR TAREAS POR FECHA LÍMITE
+console.log(`
+Ordenar las tareas por fecha límite. Debería mostrar 'Buy new house' primero y 'New year PARTY!' al final.
+`);
+console.table(project.sortTasksByDeadline());
 
+// FILTRAR TAREAS POR ESTADO Y BÚSQUEDA PALABRAS CLAVE
+console.log(`
+Filtrar tareas por estado Completed. Debería mostrar 'Give christmas presents!'.
+`);
+console.table(project.showFilteredTasks(filterTasksByStatus, "Completed"));
+console.log(`
+Filtrar tareas por estado In Progress. Debería mostrar 'Buy new car'.
+`)
+console.table(project.showFilteredTasks(filterTasksByStatus, "In Progress"));
+console.log(`
+Filtrar tareas por búsqueda de palabra clave 'buy'. Debería mostrar Tareas 'Buy new car' y 'Buy new house'.
+`);
+console.table(project.showFilteredTasks(filterTasksBySearch, "buy"));
+console.log(`
+Filtrar tareas por búsqueda de palabra clave 'Christmas'. Debería mostrar Tareas 'Give christmas presents!'.
+`);
+console.table(project.showFilteredTasks(filterTasksBySearch, "Christmas"));
+
+
+// CALCULAR TOTAL DE DIAS RESTANTE PARA COMPLETAR TODAS LAS TAREAS PENDIENTES.
+console.log(`
+Calcular el total de días restantes para completar todas las tareas pendientes.
+`);
+console.log(project.calculateRemainingTime());
+
+// CREAR ALGUNAS TAREAS CRITICAS (A MENOS DE 3 DIAS DE SU FECHA LIMITE)
 const currentDate = new Date();
 const oneDay = new Date();
 const twoDays = new Date();
@@ -208,14 +235,28 @@ threeDays.setDate(currentDate.getDate() + 2);
 project.addNewTask("Critical Task Test 01", oneDay);
 project.addNewTask("Critical Task Test 02", threeDays);
 project.addNewTask("Critical Task Test 03", twoDays);
-project.getCriticalTasks();
 
+// OBTENER LAS TAREAS CRITICAS.
+console.log(`
+Mostrar las tareas criticas (a menos de 3 días de su fecha límite y no completadas). Debería mostrar las tres Critical Task Test.
+`);
+console.table(project.getCriticalTasks());
+
+// SIMULA CARGA DE DETALLES DEL PROYECTO. A LOS 2 SEGUNDOS DEBERÍA MOSTRAR EL RESUMEN DEL PROYECTO.
 loadProjectDetails(project)
-    .then(() => {
+    .then((result) => {
+        console.table(result);
+
+        // CREA UNA TAREA CON ESTADO PENDING Y AÑADE TRES LISTENERS A ELLA.
         const taskToBeUpdated = project.addNewTask("Sell my TV", new Date(2025, 1, 15), "Pending");
         const listener1 = new TaskListener(taskToBeUpdated);
         const listener2 = new TaskListener(taskToBeUpdated);
         const listener3 = new TaskListener(taskToBeUpdated);
+
+        /* 
+        SIMULA LA ACTUALIZACIÓN DE ESTADO DE LA TAREA ANTERIOR. A LOS 2 SEGUNDOS DEBERIA ACTUALIZAR A COMPLETED.
+        AL CAMBIAR EL ESTADO DE LA TAREA A COMPLETADA, LOS TRES LISTENERS DEBERÍAN MOSTRAR UN MENSAJE EN LA CONSOLA.
+        */
         taskToBeUpdated.updateTaskStatus("Completed");
     });
 
