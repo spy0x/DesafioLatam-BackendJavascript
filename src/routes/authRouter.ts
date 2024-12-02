827477d029ef2ff5e4e3bbc287227ef52d474ad7
Francisco V. Diaz <fevarga1@uc.cl>
2024-12-01 23:59:17 -0300

    JWT fixed. Hito 1 Completed.

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -2,11 +2,13 @@ import {Router} from "express";
 import jwt from "jsonwebtoken";
 
 export const authRouter = Router();
-const jwtSecret = process.env.JWT_SECRET || "secret";
 
 authRouter.get("/register", (req, res) => {
     const {username} = req.body;
-    const token = jwt.sign({username}, jwtSecret, {expiresIn: "1h"});
-    res.send(token);
+    res.send(generateToken(username));
 });
-    
+
+const generateToken = (username: string) => {
+    const jwtSecret = process.env.JWT_SECRET || "secret";
+    return jwt.sign({username}, jwtSecret, {expiresIn: "1h"});
+}
