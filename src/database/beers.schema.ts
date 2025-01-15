3a78309a5178d75eb22575f5b39daec3e8d2c2f8
Francisco V. Diaz <fevarga1@uc.cl>
2025-01-15 09:32:26 -0300

    added sequelize schema creation. only missing changing controller and model logic

---
+++ src/database/beers.schema.ts
@@ -0,0 +1,29 @@
+import { sequelize } from "./db";
+import { DataTypes } from "sequelize";
+
+export const Beers = sequelize.define('beers', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    cerveceria: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    origen: {
+        type: DataTypes.STRING,
+    },
+    estilo: {
+        type: DataTypes.STRING,
+    },
+    alcohol: {
+        type: DataTypes.FLOAT,
+    },
+    premios: {
+        type: DataTypes.STRING,
+    },
+    ibu: {
+        type: DataTypes.INTEGER,
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express from 'express';
 import dotenv from 'dotenv';
-import {viewsRouter} from "./routes/views.route";
-import {beersRouter} from "./routes/beers.route";
-import {authRouter} from "./routes/auth.route";
+import { viewsRouter } from './routes/views.route';
+import { beersRouter } from './routes/beers.route';
+import { authRouter } from './routes/auth.route';
+import { sequelize } from './database/db';
+import './database/beers.schema';
 
 dotenv.config();
 
@@ -10,16 +12,25 @@ export const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // ROUTER
-app.use("/api/beers", beersRouter);
-app.use("/auth", authRouter);
-app.use("/", viewsRouter);
+app.use('/api/beers', beersRouter);
+app.use('/auth', authRouter);
+app.use('/', viewsRouter);
 
 // RUN SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
+const startServer = async () => {
+  try {
+    // await sequelize.authenticate();
+    await sequelize.sync({ force: true });
+    console.log('Conexión a la base de datos realizada correctamente');
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (err) {
+    console.log('Error al conectar a la base de datos', err);
+  }
+};
+startServer();
